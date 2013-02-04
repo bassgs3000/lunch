325335965b4e1f074d1a42cd0501a611e0ec1cee
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-02-04 16:18:46 -0600

    Added ssh forward agent

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ require 'capistrano/ext/multistage'
 set :application, "lunchfoo"
 set :scm, :git
 set :repository, "git@github.com:bassgs3000/lunch.git"
+set :ssh_options, {:forward_agent => true}
 set :branch, "deploy"
 set :scm_passphrase, ""
 set :user, "root"
